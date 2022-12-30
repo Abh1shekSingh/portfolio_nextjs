838cd2dcffe364b0c8bf876d58a89e0c93fc01c3
Abhishek Singh <abhinav210702@gmail.com>
2022-12-30 22:49:42 +0530

    setup tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      Hey there!
-    </>
+    
+      <h1 className='text-7xl font-bold underline'>Hey There</h1>
+    
   )
 }
