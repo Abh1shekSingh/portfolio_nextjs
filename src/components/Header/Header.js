1178f8fb0acfbd8ce6d7807494ec7f121e939e32
Abhishek Singh <abhinav210702@gmail.com>
2022-12-31 20:55:45 +0530

    added animation to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,52 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
+import {motion} from 'framer-motion'
 function Header() {
   return (
     <header className='flex flex-row sticky top-0 justify-between mx-w-7xl p-5 mx-auto xl:items-center'>
-      <div className='flex flex-row items-center'>
+      <motion.div 
+       initial ={{
+          x: -100,
+          opacity: 0,
+          scale:0.5
+       }}
+       animate={{
+          x: 0,
+          opacity: 1,
+          scale:1
+       }}
+        transition={{
+          type: 'spring',
+          stiffness: 120,
+          delay: 0.5
+        }}
+
+       className='flex flex-row items-center'>
         <SocialIcon url="https://linkedin.in/craftingbugs" fgColor='grey' bgColor='transparent' />
         <SocialIcon url="https://instagram.com/craftingbugs" fgColor='grey' bgColor='transparent' />
         <SocialIcon url="https://github.com/craftingbugs" fgColor='grey' bgColor='transparent' />
-      </div>
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      </motion.div>
+      
+      <motion.div 
+      initial ={{
+          x: 100,
+          opacity: 0,
+          scale:0.5
+       }}
+       animate={{
+          x: 0,
+          opacity: 1,
+          scale:1
+       }}
+        transition={{
+          type: 'spring',
+          stiffness: 120,
+          delay: 0.5
+        }}
+      className='flex flex-row items-center text-gray-300 cursor-pointer'>
         <SocialIcon network='email' fgColor='grey' bgColor='transparent'/>
         <p className='uppercase hidden md:inline-flex text-sm text-gray-500'>Get In Touch</p>
-      </div>
+      </motion.div>
     </header>
   )
 }
