9e8eeb27347cc4f22e54775c0e1360aaf5f47c4e
Abhishek Singh <abhinav210702@gmail.com>
2022-12-31 23:13:26 +0530

    Started About Section

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl justify-evenly px-10 mx-auto items-center'>
+        <h3 className='tracking-[10px] uppercase absolute top-24 text-xl text-[#3F9D86] '>
+            Me, Myself and I
+        </h3>
+    </div>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Hero from "../components/Hero/Hero"
+import About from "../components/About/About"
 import Work from "../components/Work/Work"
 import Project from "../components/Projects/Project"
 import {Layout} from "../layout/Layout"
@@ -8,11 +9,14 @@ export default function Home() {
   return (
     <div className="bg-[#E1ECE8] h-screen overflow-scroll snap-y snap-mandatory z-0">
       <Head>
-        <title>CRAFTINGBUGS</title>
+        <title>Craftingbugs | Developer</title>
       </Head>
       <Layout>
-        <section id = 'hero' className="snap-center">
+        <section id = 'hero' className="snap-start">
           <Hero />
+        </section>
+        <section id = 'about' className="snap-center">
+          <About />
         </section>
         
         {/* <Work />
